5ec5135fd29202df1ed8f0eb6793aa4c4b5b8c7c
Moreeh Ahmed <146959594+Moreeh97@users.noreply.github.com>
2025-08-13 15:53:19 +0300

    adding form post and print at index page

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php
 echo "Current Server Time: " . date("Y-m-d H:i:s");
+echo "<br>";
+
+?>
+    <form action="handle-form.php" method="POST">
+        <label>Name:</label>
+        <input type="text" name="username" required>
+        <label>Favorite Color:</label>
+        <input type="text" name="color" required>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
+
